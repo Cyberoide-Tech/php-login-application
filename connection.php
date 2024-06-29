6ca9804d69d7a63d2e97b139b73ac7874cd09f1e
Rehan Akhtar <61869629+rehan985@users.noreply.github.com>
2024-06-29 18:59:35 +0500

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,8 +1,13 @@
 <?php      
-    $host = "localhost";  
-    $user = "root";  
-    $password = '';  
-    $db_name = "javatpoint";  
+
+require 'load_env.php';
+loadEnv(__DIR__ . '/.env');
+
+// Get the environment variables
+        $host = getenv('DB_HOST');
+        $user = getenv('DB_USER');
+        $password = getenv('DB_PASSWORD');
+        $db_name = getenv('DB_NAME');  
       
     $con = mysqli_connect($host, $user, $password, $db_name);  
     if(mysqli_connect_errno()) {  
